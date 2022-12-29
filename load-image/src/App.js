f76cddfd704b86c00e87e84bc5026158aeb670cb
shahrzadmahami-200079 <mahamishahrzad99@gmail.com>
2022-12-30 02:02:08 +0330

    The photo display project was completed

--- load-image/src/App.js
+++ load-image/src/App.js
@@ -1,10 +1,48 @@
-
 import './App.css';
+import { Container, Card, Row } from 'react-bootstrap'
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [image, setImage] = useState([])
+  useEffect(() => {
+
+    var headersAPI = {
+      method: 'GET',
+
+      Headers: {
+        'Content-Type': 'application/json'
+        
+      },
+    }
+
+
+    const loadimage = async () => {
+      var imageData = await fetch('https://random-d.uk/api/random', headersAPI)
+        .then(Response =>
+          Response.json())
+        .then(data => {
+          setImage(data)
+        })
+    }
+    loadimage()
+  })
+
+
   return (
     <div className="App">
-      
+      <Container>
+        <Row className="center">
+          <Card className='col-10 m-5 bg-info'>
+            <Card.Img className='p-5 ' src={image.url} />
+            <Card.Body>
+              <Card.Title>
+                {image.message}
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
     </div>
   );
 }
